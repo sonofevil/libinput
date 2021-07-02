4dc1de969414b78a1b061f245090d7108e44f9bd
sonofevil <debilerpc@web.de>
2021-07-02 13:44:34 +0200

    Revert "longer debounce"

--- src/evdev-debounce.c
+++ src/evdev-debounce.c
@@ -126,7 +126,7 @@ static inline void
 debounce_set_timer(struct fallback_dispatch *fallback,
 		   uint64_t time)
 {
-	const int DEBOUNCE_TIMEOUT_BOUNCE = ms2us(120);
+	const int DEBOUNCE_TIMEOUT_BOUNCE = ms2us(25);
 
 	libinput_timer_set(&fallback->debounce.timer,
 			   time + DEBOUNCE_TIMEOUT_BOUNCE);
@@ -136,7 +136,7 @@ static inline void
 debounce_set_timer_short(struct fallback_dispatch *fallback,
 			 uint64_t time)
 {
-	const int DEBOUNCE_TIMEOUT_SPURIOUS = ms2us(60);
+	const int DEBOUNCE_TIMEOUT_SPURIOUS = ms2us(12);
 
 	libinput_timer_set(&fallback->debounce.timer_short,
 			   time + DEBOUNCE_TIMEOUT_SPURIOUS);
